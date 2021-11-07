d115b7fb8d407e40d07299aec69b6c145d5664ab
m-rejdych <rejdych.m.r@gmail.com>
2021-11-07 23:41:10 +0100

    Added path finder to cmp source config.

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -28,6 +28,7 @@ cmp.setup({
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
+    { name = 'path' },
     { name = 'vsnip' }, -- For vsnip users.
     -- { name = 'luasnip' }, -- For luasnip users.
     -- { name = 'ultisnips' }, -- For ultisnips users.
