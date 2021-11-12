4b4b5556ac1a77610e834135c4c186b9b957cac2
m-rejdych <rejdych.m.r@gmail.com>
2021-11-12 17:15:07 +0100

    Removed telescope themes.

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -8,28 +8,12 @@ vim.cmd('nnoremap <silent>gp <cmd>bp<CR>')
 local actions = require('telescope.actions')
 
 require('telescope').setup {
-  mappings = {
-    n = {
-      ["q"] = actions.close
-    },
-  },
   defaults = {
     file_ignore_patterns = {
       "node_modules",
       "build",
       "dist",
       "plugged",
-    },
-  },
-  pickers = {
-    find_files = {
-      theme = "dropdown",
-    },
-    live_grep = {
-      theme = "dropdown",
-    },
-    buffers = {
-      theme = "dropdown",
     },
   },
 }
