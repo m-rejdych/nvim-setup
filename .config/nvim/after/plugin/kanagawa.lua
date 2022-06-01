0629644a6b44cea96dc297742218b6e33b30b840
m-rejdych <rejdych.m.r@gmail.com>
2022-06-01 23:01:04 +0200

    Added theme overrides.

--- .config/nvim/after/plugin/kanagawa.lua
+++ .config/nvim/after/plugin/kanagawa.lua
@@ -1,8 +1,15 @@
+local default_colors = require('kanagawa.colors').setup()
+
+local overrides = {
+  Keyword = { fg = default_colors.peachRed, style = "bold" }
+}
+
 require('kanagawa').setup({
   keywordStyle = "bold",
   commentStyle = "NONE",
-  variablebuiltinStyle = "bold",
+  variablebuiltinStyle = "NONE",
   typeStyle = "bold",
+  overrides = overrides,
 })
 
 vim.cmd('colorscheme kanagawa')
