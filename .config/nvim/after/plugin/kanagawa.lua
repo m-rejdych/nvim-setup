98052c5255733f1a343af7c2d35f95f2e2b55349
m-rejdych <rejdych.m.r@gmail.com>
2023-01-29 23:21:32 +0100

    Added everforest colorscheme.

--- .config/nvim/after/plugin/kanagawa.lua
+++ .config/nvim/after/plugin/kanagawa.lua
@@ -1,14 +1,14 @@
-local default_colors = require('kanagawa.colors').setup()
-
-require('kanagawa').setup({
-    undercurl = false,
-    keywordStyle = { italic = false },
-    statementStyle = { bold = false },
-    variablebuiltinStyle = { italic = false },
-    transparent = true,
-    colors = {
-      sumiInk3 = default_colors.sumiInk1,
-    },
-})
-
-vim.cmd("colorscheme kanagawa")
+-- local default_colors = require('kanagawa.colors').setup()
+-- 
+-- require('kanagawa').setup({
+--     undercurl = false,
+--     keywordStyle = { italic = false },
+--     statementStyle = { bold = false },
+--     variablebuiltinStyle = { italic = false },
+--     transparent = true,
+--     colors = {
+--       sumiInk3 = default_colors.sumiInk1,
+--     },
+-- })
+-- 
+-- vim.cmd("colorscheme kanagawa")
