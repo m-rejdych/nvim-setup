9a7ecf94c781ac9eab9db7eda20b419f95a7787e
m-rejdych <rejdych.m.r@gmail.com>
2025-03-02 23:10:17 +0100

    Updated color scheme.

--- .config/nvim/after/plugin/melange.lua
+++ .config/nvim/after/plugin/melange.lua
@@ -10,3 +10,6 @@ vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
 -- Set NormalBorder (or FloatBorder) background to none
 vim.api.nvim_set_hl(0, "NormalBorder", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "DiagnosticError", { fg = "#ff5555", bold = true })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg = "#ff5555", bold = true })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { sp = "#ff5555", undercurl = true })
